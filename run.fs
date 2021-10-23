5bf6fed8bb00afe8ddceb6a0a5e6e0ad1fc80e51
NicoVIII <nico@nicoviii.net>
2021-10-23 19:17:43 +0000

    Build run in a way that propagates errors

--- run.fs
+++ run.fs
@@ -8,63 +8,129 @@ module CreateProcess =
         printfn $"> %s{cmd} %s{args'}"
         CreateProcess.fromRawCommand cmd args
 
+type ProcessResult =
+    | Ok
+    | Error
+
+module ProcessResult =
+    let toExitCode =
+        function
+        | Ok -> 0
+        | Error -> 2
+
+module Proc =
+    let combine res1 f2 =
+        match res1 with
+        | Ok -> f2 ()
+        | Error -> Error
+
+    let run (proc: CreateProcess<ProcessResult<unit>>) =
+        Proc.run proc
+        |> (fun proc ->
+            match proc.ExitCode with
+            | 0 -> Ok
+            | _ -> Error)
+
+type JobBuilder() =
+    member __.Yield x = x
+
+    member __.Combine(res1, f2) = Proc.combine res1 f2
+
+    member __.Delay f = f
+
+    member __.Run f = f ()
+
+let job = JobBuilder()
+
 module Task =
     let restore () =
         let solution =
-            Directory.EnumerateFiles(".", "*.sln")
-            |> Seq.head
+            Directory.EnumerateFiles(".", "*.sln") |> Seq.head
 
-        CreateProcess.create "dotnet" [ "tool"; "restore" ]
-        |> Proc.run |> ignore
-        CreateProcess.create "dotnet" [ "restore"; solution ]
-        |> Proc.run |> ignore
-        CreateProcess.create "pnpm" [ "install" ]
-        |> Proc.run |> ignore
+        job {
+            CreateProcess.create "dotnet" [ "tool"; "restore" ]
+            |> Proc.run
+
+            CreateProcess.create "dotnet" [ "restore"; solution ]
+            |> Proc.run
+
+            CreateProcess.create "pnpm" [ "install" ]
+            |> Proc.run
+        }
 
     let build () =
         Directory.EnumerateDirectories("modules")
-        |> Seq.iter (fun folder ->
-            CreateProcess.create "dotnet" [ "fable"; folder; "-e"; ".fs.js"; "-o"; $"%s{folder}/dist/" ]
-            |> Proc.run |> ignore
-            CreateProcess.create "pnpm" [ "exec"; "webpack-cli"; "--mode"; "production" ]
-            |> CreateProcess.withWorkingDirectory folder
-            |> Proc.run |> ignore
-        )
+        |> Seq.map (fun folder ->
+            fun () ->
+                job {
+                    CreateProcess.create
+                        "dotnet"
+                        [ "fable"
+                          folder
+                          "-e"
+                          ".fs.js"
+                          "-o"
+                          $"%s{folder}/dist/" ]
+                    |> Proc.run
+
+                    CreateProcess.create
+                        "pnpm"
+                        [ "exec"
+                          "webpack-cli"
+                          "--mode"
+                          "production" ]
+                    |> CreateProcess.withWorkingDirectory folder
+                    |> Proc.run
+                })
+        |> Seq.fold Proc.combine Ok
 
     let pack () =
         let folder = "./out"
 
-        Shell.cleanDir folder
+        job {
+            Shell.cleanDir folder
 
-        // We need the manifest, the assets and the scripts
-        Shell.copy folder [ "manifest.json" ]
-        [ "assets"; "dist" ]
-        |> List.iter (fun dir -> Shell.copyDir $"%s{folder}/%s{dir}" dir (fun _ -> true))
+            // We need the manifest, the assets and the scripts
+            Shell.copy folder [ "manifest.json" ]
 
-        // Now we can bundle stuff together
-        CreateProcess.create "pnpm" [ "exec"; "web-ext"; "build"; "-a"; folder]
-        |> CreateProcess.withWorkingDirectory folder
-        |> Proc.run |> ignore
+            [ "assets"; "dist" ]
+            |> List.iter (fun dir -> Shell.copyDir $"%s{folder}/%s{dir}" dir (fun _ -> true))
+
+            // Now we can bundle stuff together
+            CreateProcess.create
+                "pnpm"
+                [ "exec"
+                  "web-ext"
+                  "build"
+                  "-a"
+                  folder ]
+            |> CreateProcess.withWorkingDirectory folder
+            |> Proc.run
+        }
 
 module Command =
-    let restore () = Task.restore (); 0
+    let restore () = Task.restore ()
 
     let build () =
-        restore () |> ignore; Task.build (); 0
+        job {
+            restore ()
+            Task.build ()
+        }
 
-    let pack () = build() |> ignore; Task.pack(); 0
+    let pack () =
+        job {
+            build ()
+            Task.pack ()
+        }
 
 [<EntryPoint>]
 let main =
     function
-    | [| "restore" |] ->
-        Command.restore ()
+    | [| "restore" |] -> Command.restore () |> ProcessResult.toExitCode
     | [||]
-    | [| "build" |] ->
-        Command.build ()
+    | [| "build" |] -> Command.build () |> ProcessResult.toExitCode
     | [| "bundle" |]
-    | [| "pack" |] ->
-        Command.pack()
+    | [| "pack" |] -> Command.pack () |> ProcessResult.toExitCode
     | _ ->
         printfn "Invalid input"
         1
