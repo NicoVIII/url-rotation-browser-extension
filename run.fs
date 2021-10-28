c3df1e3a4ef259bc92b702265df9999ca9d88c50
Nico Linder <nl@fp.de>
2021-10-28 06:26:00 +0000

    Use RunHelpers to shorten run script

--- run.fs
+++ run.fs
@@ -1,68 +1,25 @@
-open Fake.Core
-open Fake.IO
 open System.IO
 
-module CreateProcess =
+open Fake.Core
+open Fake.IO
+
+open RunHelpers
+open RunHelpers.BasicShortcuts
+open RunHelpers.FakeHelpers
+
+module Process =
     let create cmd args = CreateProcess.fromRawCommand cmd args
-
-type ProcessResult =
-    | Ok
-    | Error
-
-module ProcessResult =
-    let toExitCode =
-        function
-        | Ok -> 0
-        | Error -> 2
-
-module Proc =
-    let combine res1 f2 =
-        match res1 with
-        | Ok -> f2 ()
-        | Error -> Error
-
-    let run (proc: CreateProcess<ProcessResult<unit>>) =
-        printfn $"> %s{proc.CommandLine}"
-
-        Proc.run proc
-        |> (fun proc ->
-            match proc.ExitCode with
-            | 0 -> Ok
-            | _ -> Error)
-
-type JobBuilder() =
-    member __.Combine(res1, f2) = Proc.combine res1 f2
-
-    member __.Delay f = f
-
-    member __.For(lst, f) =
-        lst
-        |> Seq.fold (fun res1 el -> Proc.combine res1 (fun () -> f el)) Ok
-
-    member __.Run f = f ()
-
-    member __.Yield x = x
-    member __.Zero() = Ok
-
-let job = JobBuilder()
-
-let dotnet args =
-    CreateProcess.create "dotnet" args |> Proc.run
-
-let pnpm args =
-    CreateProcess.create "pnpm" args |> Proc.run
+    let runAsJob = Proc.runAsJob Constant.errorExitCode
 
 module Task =
-    open Microsoft.VisualBasic.FileIO
-
     let restore () =
-        let solution =
-            Directory.EnumerateFiles(".", "*.sln") |> Seq.head
+        let modules =
+            Directory.EnumerateFiles("modules", "*.fsproj", SearchOption.AllDirectories)
 
         job {
-            dotnet [ "tool"; "restore" ]
-            dotnet [ "restore"; solution ]
-            pnpm [ "install" ]
+            for modul in modules do
+                Template.DotNet.restore modul
+                Template.Pnpm.install ()
         }
 
     let femto () =
@@ -77,31 +34,31 @@ module Task =
         }
 
     let build mode =
-        Directory.EnumerateFiles("modules", "*.fsproj", SearchOption.AllDirectories)
-        |> Seq.map (fun project ->
-            let folder = Path.GetDirectoryName(project)
+        let modules =
+            Directory.EnumerateFiles("modules", "*.fsproj", SearchOption.AllDirectories)
 
-            fun () ->
-                job {
-                    dotnet [ "fable"
-                             folder
-                             "-e"
-                             ".fs.js"
-                             "-o"
-                             $"%s{folder}/dist/" ]
+        job {
+            for modul in modules do
+                let folder = Path.GetDirectoryName(modul)
 
-                    CreateProcess.create
-                        "pnpm"
-                        [ "exec"
-                          "webpack-cli"
-                          "--mode"
-                          mode
-                          "-c"
-                          $"%s{folder}/webpack.config.js" ]
-                    |> CreateProcess.withWorkingDirectory folder
-                    |> Proc.run
-                })
-        |> Seq.fold Proc.combine Ok
+                dotnet [ "fable"
+                         folder
+                         "-e"
+                         ".fs.js"
+                         "-o"
+                         $"%s{folder}/dist/" ]
+
+                Process.create
+                    "pnpm"
+                    [ "exec"
+                      "webpack-cli"
+                      "--mode"
+                      mode
+                      "-c"
+                      $"%s{folder}/webpack.config.js" ]
+                |> CreateProcess.withWorkingDirectory folder
+                |> Process.runAsJob
+        }
 
     let pack () =
         let folder = "./out"
@@ -116,7 +73,7 @@ module Task =
             |> List.iter (fun dir -> Shell.copyDir $"%s{folder}/%s{dir}" dir (fun _ -> true))
 
             // Now we can bundle stuff together
-            CreateProcess.create
+            Process.create
                 "pnpm"
                 [ "exec"
                   "web-ext"
@@ -124,41 +81,38 @@ module Task =
                   "-a"
                   folder ]
             |> CreateProcess.withWorkingDirectory folder
-            |> Proc.run
+            |> Process.runAsJob
         }
 
 module Command =
     let restore () = Task.restore ()
 
-    let check_dependencies () =
+    let build () =
         job {
             restore ()
             Task.femto ()
+            Task.build "development"
         }
-
-    let build' mode =
-        job {
-            check_dependencies ()
-            Task.build mode
-        }
-
-    let build () = build' "development"
-    let build_for_prod () = build' "production"
 
     let pack () =
         job {
-            build_for_prod ()
+            restore ()
+            Task.femto ()
+            Task.build "production"
             Task.pack ()
         }
 
 [<EntryPoint>]
-let main =
-    function
-    | [| "restore" |] -> Command.restore () |> ProcessResult.toExitCode
-    | [||]
-    | [| "build" |] -> Command.build () |> ProcessResult.toExitCode
-    | [| "bundle" |]
-    | [| "pack" |] -> Command.pack () |> ProcessResult.toExitCode
-    | _ ->
-        printfn "Invalid input"
-        1
+let main args =
+    args
+    |> List.ofArray
+    |> function
+        | [ "restore" ] -> Command.restore ()
+        | []
+        | [ "build" ] -> Command.build ()
+        | [ "bundle" ]
+        | [ "pack" ] -> Command.pack ()
+        | _ ->
+            let msg = [ "Invalid input" ]
+            Error(1, msg)
+    |> ProcessResult.wrapUp
